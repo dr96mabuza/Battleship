f4d765e4ffd5f3adb563ce073b934150c4c189e3
dr96mabuza <dr96mabuza@gmail.com>
2022-04-25 16:00:43 +0200

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -23,54 +20,6 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-      {
-        test: /\.toml$/i,
-        type: "json",
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
-    loaders: [
-      { exclude: ["node_modules"], loader: "babel", test: /\.jsx?$/ },
-      { loader: "style-loader!css-loader", test: /\.css$/ },
-      { loader: "url-loader", test: /\.gif$/ },
-      { loader: "file-loader", test: /\.(ttf|eot|svg)$/ },
-    ],
-  },
-  resolve: {
-    alias: {
-      config$: "./configs/app-config.js",
-      react: "./vendor/react-master",
-    },
-    extensions: ["", "js", "jsx"],
-    modules: [
-      "node_modules",
-      "bower_components",
-      "shared",
-      "/shared/vendor/modules",
     ],
   },
 };
