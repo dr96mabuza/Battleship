01949960fe9be23778b4b3d9de5436d21be4faa5
sifiso <simabuz022@student.wethinkcode.co.za>
2023-07-19 12:05:17 +0200

    created place ship method

--- src/__tests__/gameBoard.test.js
+++ src/__tests__/gameBoard.test.js
@@ -1,8 +1,19 @@
-const gameBoard = require("./../components/gameBoard");
+import gameBoard from "./../components/gameBoard";
+import ship from "./../components/ship";
 
 let board = gameBoard();
 
-test("", () => {
-    expect(board).toStrictEqual({})
+test("test game board.", () => {
+    expect(typeof board.getBoard()).toStrictEqual("object");
+    expect(Object.keys(board.getBoard()).length).toStrictEqual(50);
+    board.getBoard().forEach(element => {
+        expect(typeof element).toBe("object");
+    });
+
 });
-// test("", () => {});
+test("test place ship into game board.", () => {
+    board.placeShip(ship("battleship"), "y", 1, 3);
+    board.getBoard().forEach((element) => {
+        element.length > 0 ? expect(typeof element[0]).toBe("object") : null; 
+    });
+});
--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,6 +1,52 @@
 const gameBoard = () => {
-    // const size = size;
-    return {};
+
+    /**
+     * create a board with 50 positions.
+     * @returns { Array }
+     */
+    const generateBoard = () => {
+        let array = [];
+        for (let index = 0; index < 50; index++) {
+            array.push([])
+        }
+        return array;
+    };
+
+    let board = generateBoard();
+
+    /**
+     * get the game board with the latest data.
+     * @returns { gameBoard }
+     */
+    const getBoard = () => { return board; };
+
+    /**
+     * place the ship selected by the user in the board.
+     * @param { ship } shipObject 
+     * @param { String } axis 
+     * @param { Number } index // index of the array inside the board array.
+     * @param { Number } startLocation // column index
+     */
+    const placeShip = (shipObject, axis, index, startLocation) => {
+
+        /**
+         * if the ship size is within the array length, 
+         * place the ship from the starting position until the end position.
+         * ship will be contained within one inner array.
+         */
+        const yAxisPacement = () => {
+
+        };
+
+        switch(axis) {
+            case "y":
+                break;
+            case "x":
+                break;
+        }
+    };
+
+    return {getBoard, placeShip};
 }
 
-module.exports = gameBoard;
+export default gameBoard;
