2cdd54528772b12a165e353dac177e2b718a284f
sifiso <simabuz022@student.wethinkcode.co.za>
2023-07-27 15:34:32 +0200

    moved random getter code to own functions

--- src/components/player.js
+++ src/components/player.js
@@ -1,26 +1,30 @@
 import gameBoard from "./gameBoard";
 
 const player = ( name ) => {
-    
+
     const board = gameBoard();
     const getName = () => name;
     const getPlayerBoard = () => board; 
 
-    const generateRandomIndex = () => Math.random() * 10;
+    const generateRandomIndex = () => Math.floor(Math.random() * 10);
+    const getRandomShip = () => (
+        [
+            "battleship",
+            "carrier",
+            "cruiser",
+            "submarine",
+            "destroyer"
+        ][Math.floor(Math.random() * 4)]
+    );
+    const getRandomAxis = () => ["x", "y"][Math.floor(Math.random() * 2)];
 
     function randomlyPlaceShips() {
         // const aiShipsLocations = [];
         while (!board.allPlaced()) {
             const outerIndex = generateRandomIndex();
             const innerIndex = generateRandomIndex();
-            const axis = ["x", "y"][Math.random() * 1];
-            const ship = [
-                "battleship",
-                "carrier",
-                "cruiser",
-                "submarine",
-                "destroyer"
-            ][Math.random() * 4];
+            const axis = getRandomAxis();
+            const ship = getRandomShip();
             if (board.placeShip(ship, axis, outerIndex, innerIndex)) {
                 board.placeShip(ship, axis, outerIndex, innerIndex);
             }
