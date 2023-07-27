a464445bef257952d3f158ea089d4322ecbbbefc
sifiso <simabuz022@student.wethinkcode.co.za>
2023-07-27 15:22:36 +0200

    base tests

--- src/__tests__/player.test.js
+++ src/__tests__/player.test.js
@@ -1,4 +1,21 @@
 /* eslint-disable no-undef */
 import player from "../components/player";
 
-test("", () => {});
+test("create new player", () => {
+    expect(typeof player("computer")).toBe("object");
+});
+
+test("test create player. returns player name.", () => {
+    expect(player("computer").getName()).toBe("computer");
+    expect(player("paul").getName()).toBe("paul");
+});
+
+test("test if player returns player board.", () => {
+    expect(typeof player("paul").getPlayerBoard()).toBe("object");
+});
+
+test("place ships randomly if computer is playing", () => {
+    const ai = player("computer");
+    ai.randomlyPlaceShips();
+    expect(ai.getPlayerBoard().allPlaced()).toBeTruthy()
+});
--- src/components/player.js
+++ src/components/player.js
@@ -1,3 +1,45 @@
-const player = () => ({})
+import gameBoard from "./gameBoard";
+
+const player = ( name ) => {
+    
+    const board = gameBoard();
+    const getName = () => name;
+    const getPlayerBoard = () => board; 
+
+    const generateRandomIndex = () => Math.random() * 10;
+
+    function randomlyPlaceShips() {
+        // const aiShipsLocations = [];
+        while (!board.allPlaced()) {
+            const outerIndex = generateRandomIndex();
+            const innerIndex = generateRandomIndex();
+            const axis = ["x", "y"][Math.random() * 1];
+            const ship = [
+                "battleship",
+                "carrier",
+                "cruiser",
+                "submarine",
+                "destroyer"
+            ][Math.random() * 4];
+            if (board.placeShip(ship, axis, outerIndex, innerIndex)) {
+                board.placeShip(ship, axis, outerIndex, innerIndex);
+            }
+        }
+    };
+
+    if (getName() === "computer") {
+        return { 
+            getName, 
+            getPlayerBoard, 
+            randomlyPlaceShips 
+        }
+    };
+
+    return { 
+        getName, 
+        getPlayerBoard, 
+    }
+
+};
 
 export default player;
