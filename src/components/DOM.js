77b0e2d474127dd1cc89d14ea4cfa78bb9030553
Sifiso <dr96mabuza@gmail.com>
2023-08-01 21:22:24 +0200

    fix column and row gen

--- src/components/DOM.js
+++ src/components/DOM.js
@@ -1,15 +1,20 @@
+const createDiv = (id) => {
+    const div = document.createElement("div");
+    div.setAttribute("id", id);
+    return div;
+};
+
 const renderBoard = ( board ) => {
     const body = document.querySelector("body");
-    const boardHolder =  document.createElement("div");
-    console.log(board)
+    const boardHolder =  createDiv("board");
+    // console.log(board)
     board.forEach(element => {
-        const column = document.createElement("div");
-        column.setAttribute("id", "board-column");
+        const column = createDiv("board-column");
+        column.style.border = "solid black 2px";
 
         element.forEach(item => {
-            const row = document.createElement("div");
-            row.setAttribute("id", "board-row");
-
+            const row = createDiv("board-row");
+            row.textContent = item;
             column.appendChild(row);
         })
 
